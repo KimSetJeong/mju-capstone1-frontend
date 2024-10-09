178fbb1d6ec0fd734ca4a1a6c49d23e45bd3e2e0
kkimhyorr <hyorie03@naver.com>
2024-10-09 23:10:20 +0900

    Feat: Header 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import { GlobalFont } from './styles/GlobalFont';
+import GlobalStyle from './styles/GlobalStyle';
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalFont />
+      <GlobalStyle />
+      <Router>
+        <Header />
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
