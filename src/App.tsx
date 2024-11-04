ad080057a1747669d74b170cc2661d527efe696b
kkimhyorr <hyorie03@naver.com>
2024-11-05 00:40:41 +0900

    Feat: Error 페이지 구현완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import { GlobalFont } from './styles/GlobalFont';
+import GlobalStyle from './styles/GlobalStyle';
+import Error from './pages/Error';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalFont />
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/error" element={<Error />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Error.tsx
@@ -0,0 +1,58 @@
+import { ReactComponent as Light } from '../assets/images/Error/BlightCircle.svg';
+import { ReactComponent as Dark } from '../assets/images/Error/DarkCircle.svg';
+import { styled } from 'styled-components';
+
+const Error = () => {
+  return (
+    <Container>
+      <TextWrapper>
+        <Title>ERROR</Title>
+        <SubTitle>페이지를 찾을 수 없습니다.</SubTitle>
+      </TextWrapper>
+      <LightCircle />
+      <DarkCircle />
+    </Container>
+  );
+};
+
+export default Error;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const LightCircle = styled(Light)`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const DarkCircle = styled(Dark)`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 13px;
+`;
+
+const Title = styled.p`
+  font-weight: 800;
+  font-size: 120px;
+  color: ${({ theme }) => theme.color.BLUE700};
+`;
+
+const SubTitle = styled.p`
+  font-weight: 800;
+  font-size: 50px;
+  color: ${({ theme }) => theme.color.BLUE700};
+`;
