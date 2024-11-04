4883b38b3204679c40235c89596bbbc732df7ee6
kkimhyorr <hyorie03@naver.com>
2024-11-05 01:02:06 +0900

    Feat: Loading 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import { GlobalFont } from './styles/GlobalFont';
+import GlobalStyle from './styles/GlobalStyle';
+import Loading from './pages/Loading';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalFont />
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/loading" element={<Loading />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
