dff1cdaf1b8fcc324d1fc1320729f8f3e88723d7
kkimhyorr <hyorie03@naver.com>
2024-11-13 02:03:17 +0900

    Feat: 유효성 검사 구현

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,28 +1,104 @@
 import { styled } from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
 
-const SignUp = () => {
+type FormValues = {
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const SignUp: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    trigger,
+  } = useForm<FormValues>({
+    mode: 'onChange',
+  });
+
+  useEffect(() => {
+    trigger();
+  }, [trigger]);
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
+  const password = watch('password');
+
   return (
-    <Container>
-      <Title>회원가입</Title>
-      <div>
-        <IdWrapper>
-          <IdInput placeholder="ID" />
-          <IdBtn>Check</IdBtn>
-        </IdWrapper>
-        <InputGuide>알파벳과 숫자만을 이용하여 3 ~ 12자</InputGuide>
-      </div>
-      <div>
-        <PWInput placeholder="Password" type="password" />
-        <InputGuide>알파벳과 숫자만을 이용하여 6 ~ 14자</InputGuide>
-      </div>
-      <div>
-        <PWInput placeholder="Confirm Password" />
-        <InputGuide>
-          위와 동일하게 비밀번호를 한 번 더 입력해주세요 !
-        </InputGuide>
-      </div>
-      <OKBtn>OK</OKBtn>
-    </Container>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container>
+        <Title>회원가입</Title>
+        <div>
+          <IdWrapper>
+            <IdInput
+              placeholder="ID"
+              type="text"
+              {...register('username', {
+                required: '알파벳과 숫자만을 이용하여 3 ~ 12자',
+                minLength: {
+                  value: 3,
+                  message: '아이디는 3자 이상이어야 합니다.',
+                },
+                maxLength: {
+                  value: 12,
+                  message: '아이디는 12자 이하여야 합니다.',
+                },
+                pattern: {
+                  value: /^[A-Za-z0-9]+$/,
+                  message: '아이디는 알파벳과 숫자만 포함할 수 있습니다.',
+                },
+              })}
+            />
+            <IdBtn>Check</IdBtn>
+          </IdWrapper>
+          <InputGuide>{errors.username && errors.username.message}</InputGuide>
+        </div>
+        <div>
+          <PWInput
+            placeholder="Password"
+            type="password"
+            {...register('password', {
+              required: '알파벳과 숫자만을 이용하여 6 ~ 14자',
+              minLength: {
+                value: 6,
+                message: '비밀번호는 6자 이상이어야 합니다.',
+              },
+              maxLength: {
+                value: 14,
+                message: '비밀번호는 14자 이하여야 합니다.',
+              },
+              pattern: {
+                value: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,14}$/,
+                message: '비밀번호는 영문과 숫자가 모두 포함되어야 합니다.',
+              },
+            })}
+          />
+          {errors.password && (
+            <InputGuide>{errors.password.message}</InputGuide>
+          )}
+        </div>
+        <div>
+          <PWInput
+            placeholder="Confirm Password"
+            type="password"
+            {...register('confirmPassword', {
+              required: '위와 동일하게 비밀번호를 한 번 더 입력해주세요 !',
+              validate: (value) =>
+                value === password || '비밀번호가 일치하지 않습니다.',
+            })}
+          />
+          {errors.confirmPassword && (
+            <InputGuide>{errors.confirmPassword.message}</InputGuide>
+          )}
+        </div>
+        <OKBtn type="submit">OK</OKBtn>
+      </Container>
+    </form>
   );
 };
 
